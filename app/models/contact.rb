96cde674b7842420e8a8ab41df6ac983adda1319
ianfletcher55 <ian.fletcher55@gmail.com>
2020-06-03 11:54:35 -0500

    adds partial

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,21 @@
 class Contact < ApplicationRecord
+
+  def friendly_created_at
+    created_at.strftime("%b %e, %l:%M %p")
+  end
+
+  def friendly_updated_at
+    updated_at.strftime("%b %e, %l:%M %p")
+  end
+
+  def full_name
+    result = "#{first_name} #{last_name}"
+    result
+  end
+
+  def country_code
+    result = "+81 #{phone_number}"
+    result
+  end
+
 end
